858943c8aab162de23e7bd663520b9e957bb313d
visvaden7 <visvaden7@naver.com>
2024-08-28 10:03:46 +0900

    feat: Layout 구성
    레이아웃 구성 및 1차 화면 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import {Fragment} from 'react'
-import { createRoot } from 'react-dom/client'
+import {createRoot} from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {BrowserRouter} from "react-router-dom";
 
 createRoot(document.getElementById('root')!).render(
-  <Fragment>
-    <App />
-  </Fragment>,
+    <Fragment>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Fragment>,
 )
