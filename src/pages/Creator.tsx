1d833d1ccaa4fa170a916bfcee5e2a069f43b764
KK836 <drwk1640@gmail.com>
2024-09-24 16:26:35 +0900

    refactor: DramaCarousel 화살표 위치 Creator 이미지 모서리 둥글게 BarChart 프론트 API 연결 AnalyzeData css 개선
    - DramaCarousel 화살표 위치 변경
    - Creator 이미지 모서리 둥글게 적용
    - BarChart 프론트 API 연결
    - AnalyzeData  css 개선 및 class 'container' style 2600px 적용

--- src/pages/Creator.tsx
+++ src/pages/Creator.tsx
@@ -2,7 +2,6 @@ import { FunctionComponent, useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom"; // useLocation 추가
 import "./Creator.css";
 import axios from "axios";
-import {API_ENDPOINT} from "../assets/const/constant.ts";
 
 type CreatorRole = "writer" | "director";
 
@@ -50,7 +49,7 @@ export const Creator: FunctionComponent = () => {
       setError(null);
       try {
         const response = await axios.get(
-          `${API_ENDPOINT}/creator/getAllCreator`
+          `http://localhost:8001/api/creator/getAllCreator`
         );
 
         if (response.status !== 200) {
