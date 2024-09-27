224ce7f0c820c619b2cce2f23f91c6a2fb10e0b0
visvaden7 <visvaden7@naver.com>
2024-09-28 02:13:18 +0900

    refactor: logout test in web

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.tsx";
 import "./index.css";
 import { App } from "./App.tsx";
+import {ScrollTop} from "./components/ScrollTop.tsx";
 
 const rootElement = document.getElementById("root");
 
 if (rootElement) {
   createRoot(rootElement).render(
     <BrowserRouter>
+      <ScrollTop/>
       <AuthProvider>
         <App />
       </AuthProvider>
